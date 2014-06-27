6eb3d374288e14c6012503dcb1e3ac390259d49e
Брежнев Иван <brezhnev.ivan@yahoo.com>
2014-06-27 22:31:40 +0300

    Fix indentation

--- app/modules/moduleRegistry/ModuleRegistryServiceProvider.php
+++ app/modules/moduleRegistry/ModuleRegistryServiceProvider.php
@@ -2,15 +2,15 @@
 
 class ModuleRegistryServiceProvider extends \Illuminate\Support\ServiceProvider {
 
-	public function register()
-	{
-		$this->app->booting(function()
-		{
-			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+    public function register()
+    {
+        $this->app->booting(function()
+        {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
             $loader->alias('ModuleRegistry', 'App\Modules\ModuleRegistry\Facades\ModuleRegistryFacade');
             \ModuleRegistry::registryAdminModule("Module");
 
             // Artisan::call('dump-autoload');
-		});
-	}
+        });
+    }
 }
