37e76e47a6e732d30548b7229ee4240c0ca8bab3
Andrey <QuissR@gmail.com>
2014-06-30 01:24:45 +0400

    Admin panel base scelet UI

--- app/modules/moduleRegistry/models/ModuleRegistry.php
+++ app/modules/moduleRegistry/models/ModuleRegistry.php
@@ -1,21 +1,28 @@
 <?php namespace App\Modules\ModuleRegistry\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
 class ModuleRegistry {
 
     protected static $adminModule = array();
 
-    public static function registryAdminModule($module, $facade = null)
-    {
-        if ($facade != null) $module = array($module, $facade);
-        array_push(self::$adminModule, $module);
+    protected static $moduleSettings = array(
+        'administrator.settings' => true,
+    );
 
+    public static function registryAdminModule($module,$facade=null) {
+        if($facade != null) $module = array($module,$facade);
+        else $module = array($module);
+        array_push(self::$adminModule,$module);
         return true;
     }
 
-    public static function getAdminModule()
-    {
+    public static function getAdminModule() {
         return self::$adminModule;
+    }
+
+    public static function getSettings($section) {
+        if(in_array($section,self::$moduleSettings))
+            return true;
+        else
+            return false;
     }
 }
--- app/views/administrator/settings.blade.php
+++ app/views/administrator/settings.blade.php
@@ -1,9 +1,10 @@
 @extends('administrator.layout')
 
 @section('content')
+    <h2>Gene</h2>
+    <h2>Active other module:</h2>
     @foreach($modules as $module)
         <div class="ui icon header">
-            <i class="circular question icon"></i>
             <a href="{{ $module }}">{{ $module }}</a>
         </div>
     @endforeach
